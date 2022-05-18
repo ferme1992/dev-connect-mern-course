62dffd9e50e01de6ea5c985d1809e7401b898c07
Fernando Estevez <ferme1992@gmail.com>
2022-05-18 16:02:03 -0300

    Connection to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express= require('express');
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
