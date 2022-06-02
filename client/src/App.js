27a821914840b62d4e32f0a8f10930fec16388f7
Fernando Estevez <ferme1992@gmail.com>
2022-06-02 19:28:43 -0300

    profile and profiles

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,41 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import ProfileForm from './components/profile-forms/ProfileForm';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import PrivateRoute from './components/routing/PrivateRoute';
-//Redux
+import { LOGOUT } from './actions/types';
+
+// Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
+import './App.css';
 
 const App = () => {
   useEffect(() => {
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token);
+    }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
     store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
   }, []);
 
   return (
@@ -36,6 +48,7 @@ const App = () => {
             <Route path='/' element={<Landing />} />
             <Route path='/register' element={<Register />} />
             <Route exact path='/login' element={<Login />} />
+            <Route path='profiles' element={<Profiles />} />
             <Route
               path='dashboard'
               element={<PrivateRoute component={Dashboard} />}
