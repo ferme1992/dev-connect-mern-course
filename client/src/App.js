8b52a0cd106e7a862d8d08f2694669ccbb06c375
Fernando Estevez <ferme1992@gmail.com>
2022-06-02 21:41:48 -0300

    post and comments

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,10 @@ import ProfileForm from './components/profile-forms/ProfileForm';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+import NotFound from './components/layout/NotFound';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { LOGOUT } from './actions/types';
 
@@ -41,36 +45,38 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <>
-          <Navbar />
-          <Alert />
-          <Routes>
-            <Route path='/' element={<Landing />} />
-            <Route path='/register' element={<Register />} />
-            <Route exact path='/login' element={<Login />} />
-            <Route path='profiles' element={<Profiles />} />
-            <Route
-              path='dashboard'
-              element={<PrivateRoute component={Dashboard} />}
-            />
-            <Route
-              path='create-profile'
-              element={<PrivateRoute component={ProfileForm} />}
-            />
-            <Route
-              path='edit-profile'
-              element={<PrivateRoute component={ProfileForm} />}
-            />
-            <Route
-              path='add-experience'
-              element={<PrivateRoute component={AddExperience} />}
-            />
-            <Route
-              path='add-education'
-              element={<PrivateRoute component={AddEducation} />}
-            />
-          </Routes>
-        </>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+          <Route path='profiles' element={<Profiles />} />
+          <Route path='profile/:id' element={<Profile />} />
+          <Route
+            path='dashboard'
+            element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route
+            path='create-profile'
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path='edit-profile'
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path='add-experience'
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path='add-education'
+            element={<PrivateRoute component={AddEducation} />}
+          />
+          <Route path='posts' element={<PrivateRoute component={Posts} />} />
+          <Route path='posts/:id' element={<PrivateRoute component={Post} />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
       </Router>
     </Provider>
   );
